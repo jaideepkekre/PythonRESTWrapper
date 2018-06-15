90997951f9a250aa5e9a9c87bab930883dc23437
Jaideep Kekre <jaideepkekre@users.noreply.github.com>
2018-06-15 13:00:26 +0530

    Update simpleFlaskApp.py

--- simpleFlaskApp.py
+++ simpleFlaskApp.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template, make_response, request, Response 
-
-app = Flask(__name__)
-
-# Modification 2: Define your own Flask app routes and functions
-@app.route('/')
-def index():
-    return ("mew")
+#!/usr/bin/python
+from flask import request, Response
+from flask import Flask
+import json
 
 
-def getAPP():
-    return app
+
+APP = Flask(__name__)
+
+
+
+@APP.route('/', methods=['POST'])
+def route_function():
+    """
+    Function to handle "/" route POST call. Can create multiple functions to handle multiple routes.
+    """
+    body = request.json
+    return Response(json.dumps(body), status=200, mimetype='application/json')
+
+    
+
+
+if __name__ == '__main__':
+    APP.run(host="0.0.0.0", port=8080, threaded=True)
+#gunicorn  -w 10 --threads 12 sampleFlaskApp:APP
